7638101f014c55da501d8f70b008897519a395ef
Vladislav <Glagli@yandex.ru>
2021-09-08 22:16:30 +0300

    HomeWork_lesson1

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -1,3 +1,70 @@
-public class HomeWorkApp {
+/*
 
+1. Создать пустой проект в IntelliJ IDEA, создать класс HomeWorkApp, и прописать в нем метод main().
+
+2. Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple.
+Orange
+Banana
+Apple
+
+3. Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b, и инициализируйте их любыми значениями, которыми захотите.
+Далее метод должен просуммировать эти переменные, и если их сумма больше или равна 0,
+то вывести в консоль сообщение “Сумма положительная”, в противном случае - “Сумма отрицательная”;
+
+
+4. Создайте метод printColor() в теле которого задайте int переменную value и инициализируйте ее любым значением.
+Если value меньше 0 (0 включительно),
+то в консоль метод должен вывести сообщение “Красный”,
+если лежит в пределах от 0 (0 исключительно) до 100 (100 включительно), то “Желтый”,
+если больше 100 (100 исключительно) - “Зеленый”;
+
+5. Создайте метод compareNumbers(), в теле которого объявите две int переменные a и b,
+и инициализируйте их любыми значениями, которыми захотите. Если a больше или равно b,
+то необходимо вывести в консоль сообщение “a >= b”, в противном случае “a < b”;
+
+6. Методы из пунктов 2-5 вызовите из метода main() и посмотрите корректно ли они работают.
+
+*/
+
+
+public class HomeWorkApp {
+    public static String printThreeWords() {
+        return "Orange\n" +
+                "Banana\n" +
+                "Apple\n";
+
+    }
+
+    public static void checkSumSign(int a, int b) {
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная = " + (a + b));
+        } else {
+            System.out.println("Сумма отрицательная = " + (a + b));
+        }
+    }
+
+    public static void printColor(int value) {
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 & value <= 100) {
+            System.out.println("Желтый");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers(int a, int b) {
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(printThreeWords());
+        checkSumSign(10, 20);
+        printColor(1);
+        compareNumbers(770, 770);
+    }
 }
